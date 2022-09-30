3358a540b49cf8262a106e43572132cb6dfbd80e
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-09-30 10:51:28 -0400

    FIxing some issues

--- src/use-cases/create-order.spec.ts
+++ src/use-cases/create-order.spec.ts
@@ -2,5 +2,35 @@ import { test, expect } from "vitest";
 import { Order } from "../entities/order";
 import { CreateOrder } from "./create-order";
 test("A client shoudn't be able to create more than one order", () => {
-  
+  const client = {
+    location: {
+      address: "Rua Frei Emiliano Monteiro",
+      bairro: "Carumbé",
+      casa: "30",
+      cep: "78050-690",
+      reference: "Atrás dos prédios MRV, casa de portões vermelhos",
+    },
+    name: "Felipe Aguiar",
+    phone: "(65) 99239-1561",
+  };
+  const order_to_error = new Order(28, 8, -1, client, ["A"], "Dinheiro, troco para R$50");
+  const create_order = new CreateOrder(order_to_error);
+  expect(create_order.execute()).resolves.toThrowError("The same client can't make more than one order");
+});
+
+test("An address shoudn't be able to receive more than one order", () => {
+  const client = {
+    location: {
+      address: "Rua Frei Emiliano Monteiro",
+      bairro: "Carumbé",
+      casa: "30",
+      cep: "78050-690",
+      reference: "Any reference",
+    },
+    name: "Felipe Ferreira",
+    phone: "(65) 99239-1562",
+  };
+  const order_to_error = new Order(28, 8, -1, client, ["A"], "Dinheiro, troco para R$50");
+  const create_order = new CreateOrder(order_to_error);
+  expect(create_order.execute()).resolves.toThrowError("Can't have more than two orders to the same address");
 });
--- src/use-cases/create-order.ts
+++ src/use-cases/create-order.ts
@@ -4,19 +4,24 @@ import { Order } from "../entities/order";
 import { DB } from "../firebase";
 
 export class CreateOrder {
-  constructor(private order: Order) {
-    const orders = getDocs(collection(DB, "Orders")) as Promise<QuerySnapshot<Order>>;
-    orders.then((snapshots) => {
-      snapshots.docs.forEach((doc) => {
-        const docData = doc.data();
-        if (docData.client.phone === this.order.client.phone) {
-          throw new Error("The same client can't make more than one order");
-        }
-        if (isEqual(docData.client.location, this.order.client.location)) {
-          throw new Error("Can't have more than two orders to the same address");
-        }
-      });
+  private readonly orders = getDocs(collection(DB, "Orders")) as Promise<QuerySnapshot<Order>>;
+  constructor(private order: Order) {}
+  private async checkValidity() {
+    const docs = (await this.orders).docs;
+    docs.forEach((doc) => {
+      const docData = doc.data();
+      if (docData.client.phone === this.order.client.phone) {
+        throw new Error("The same client can't make more than one order");
+      }
+      if (isEqual(docData.client.location, this.order.client.location)) {
+        throw new Error("Can't have more than two orders to the same address");
+      }
     });
+    return true;
   }
-  public execute() {}
+  public async execute() {
+    if (await this.checkValidity()) {
+      // Implementation code
+    }
+  }
 }
