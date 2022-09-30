cdb1ddee73b0957fbd4462cd7b27df2fa4596328
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-09-30 10:56:43 -0400

    Improving location check

--- src/use-cases/create-order.ts
+++ src/use-cases/create-order.ts
@@ -10,10 +10,20 @@ export class CreateOrder {
     const docs = (await this.orders).docs;
     docs.forEach((doc) => {
       const docData = doc.data();
+      const docLocation = {
+        address: docData.client.location.address,
+        bairro: docData.client.location.bairro,
+        casa: docData.client.location.casa,
+      };
+      const clientLocation = {
+        address: this.order.client.location.address,
+        bairro: this.order.client.location.bairro,
+        casa: this.order.client.location.casa,
+      };
       if (docData.client.phone === this.order.client.phone) {
         throw new Error("The same client can't make more than one order");
       }
-      if (isEqual(docData.client.location, this.order.client.location)) {
+      if (isEqual(docLocation, clientLocation)) {
         throw new Error("Can't have more than two orders to the same address");
       }
     });
