c702038f26e5b6b18477eded8474ce7dd622fb4c
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-09-30 17:22:30 -0400

    Improving the CreateOrder class and it tests

--- src/use-cases/create-order.spec.ts
+++ src/use-cases/create-order.spec.ts
@@ -1,7 +1,21 @@
+import { uniqueId } from "lodash";
 import { test, expect } from "vitest";
 import { Order } from "../entities/order";
 import { CreateOrder } from "./create-order";
 test("A client shoudn't be able to create more than one order", () => {
+  const first_client = {
+    location: {
+      address: "Avenida Jurumirim",
+      bairro: "Bela Vista",
+      casa: "30",
+      cep: "78050-194",
+      reference: "Any reference",
+    },
+    name: "Felipe Ferreira",
+    phone: "(65) 99239-1561",
+  };
+  const first_order = new Order(28, 8, -1, first_client, ["AA"], "Dinheiro, troco para R$200");
+  const inMemoryRepository = [first_order];
   const client = {
     location: {
       address: "Rua Frei Emiliano Monteiro",
@@ -13,13 +27,13 @@ test("A client shoudn't be able to create more than one order", () => {
     name: "Felipe Aguiar",
     phone: "(65) 99239-1561",
   };
-  const order_to_error = new Order(28, 8, -1, client, ["A"], "Dinheiro, troco para R$50");
-  const create_order = new CreateOrder(order_to_error);
-  expect(create_order.execute()).resolves.toThrowError("The same client can't make more than one order");
+  const order_for_error = new Order(28, 8, -1, client, ["A"], "Dinheiro, troco para R$50");
+  expect(() => new CreateOrder(order_for_error, inMemoryRepository)).toThrowError(
+    "The same client can't make more than one order"
+  );
 });
-
-test("An address shoudn't be able to receive more than one order", () => {
-  const client = {
+test("The same location can't receive more than one order", () => {
+  const first_client = {
     location: {
       address: "Rua Frei Emiliano Monteiro",
       bairro: "Carumbé",
@@ -28,9 +42,23 @@ test("An address shoudn't be able to receive more than one order", () => {
       reference: "Any reference",
     },
     name: "Felipe Ferreira",
+    phone: "(65) 99239-1561",
+  };
+  const first_order = new Order(28, 8, -1, first_client, ["AA"], "Dinheiro, troco para R$75");
+  const inMemoryRepository = [first_order];
+  const client = {
+    location: {
+      address: "Rua Frei Emiliano Monteiro",
+      bairro: "Carumbé",
+      casa: "30",
+      cep: "78050-690",
+      reference: "Atrás dos prédios MRV, casa de portões vermelhos",
+    },
+    name: "Felipe Aguiar",
     phone: "(65) 99239-1562",
   };
-  const order_to_error = new Order(28, 8, -1, client, ["A"], "Dinheiro, troco para R$50");
-  const create_order = new CreateOrder(order_to_error);
-  expect(create_order.execute()).resolves.toThrowError("Can't have more than two orders to the same address");
+  const order_for_error = new Order(28, 8, -1, client, ["A"], "Dinheiro, troco para R$50");
+  expect(() => new CreateOrder(order_for_error, inMemoryRepository)).toThrowError(
+    "Can't have more than two orders to the same address"
+  );
 });
--- src/use-cases/create-order.ts
+++ src/use-cases/create-order.ts
@@ -1,37 +1,28 @@
-import { collection, getDocs, QuerySnapshot, where } from "firebase/firestore";
 import { isEqual } from "lodash";
 import { Order } from "../entities/order";
-import { DB } from "../firebase";
 
 export class CreateOrder {
-  private readonly orders = getDocs(collection(DB, "Orders")) as Promise<QuerySnapshot<Order>>;
-  constructor(private order: Order) {}
-  private async checkValidity() {
-    const docs = (await this.orders).docs;
-    docs.forEach((doc) => {
-      const docData = doc.data();
-      const docLocation = {
-        address: docData.client.location.address,
-        bairro: docData.client.location.bairro,
-        casa: docData.client.location.casa,
+  constructor(private readonly order: Order, orderRepository: Order[]) {
+    const orderLocationForCheck = {
+      address: order.client.location.address,
+      bairro: order.client.location.bairro,
+      casa: order.client.location.casa,
+    };
+    orderRepository.forEach((orderData) => {
+      const repoLocationForCheck = {
+        address: orderData.client.location.address,
+        bairro: orderData.client.location.bairro,
+        casa: orderData.client.location.casa,
       };
-      const clientLocation = {
-        address: this.order.client.location.address,
-        bairro: this.order.client.location.bairro,
-        casa: this.order.client.location.casa,
-      };
-      if (docData.client.phone === this.order.client.phone) {
+      if (orderData.client.phone === order.client.phone) {
         throw new Error("The same client can't make more than one order");
       }
-      if (isEqual(docLocation, clientLocation)) {
+      if (isEqual(repoLocationForCheck, orderLocationForCheck)) {
         throw new Error("Can't have more than two orders to the same address");
       }
     });
-    return true;
   }
   public async execute() {
-    if (await this.checkValidity()) {
-      // Implementation code
-    }
+    // Create the order inside here (No extra verification, only creates it)
   }
 }
