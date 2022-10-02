604f6e6a598c517f24144dea98a33534d33518ca
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-10-02 19:51:19 -0400

    Color lib

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { cyan, bgBlue } from "colors/safe";
 import { routes } from "./server/routes";
 import { express_app } from "./server/server";
 const port = process.env.PORT || 3333;
 express_app.use(routes);
+
 express_app.listen(port, () => {
-  console.log(`Server initialized in port ${port}`);
+  console.log(cyan(`Server initialized in port ${port}`));
 });
