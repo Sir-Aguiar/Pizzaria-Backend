ad04fdac7c88488a130d840954169c4ed59877ec
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-10-03 15:48:13 -0400

    update status sytem

--- src/use-cases/update-order.ts
+++ src/use-cases/update-order.ts
@@ -7,7 +7,9 @@ import { DB } from "../firebase";
 export class UpdateOrder {
   constructor(private readonly order: Order, private readonly employee: EmployeeCredential) {}
   public async changeOrderStatus(new_status: number) {
-    // Implements the code in here
+    await updateDoc(doc(DB, "Orders", this.order.id), {
+      status: new_status,
+    });
   }
   public async changeDeliveryPrice(new_delivery: number) {
     if (this.order.status != -1) {
@@ -23,17 +25,3 @@ export class UpdateOrder {
     });
   }
 }
-const client = {
-  location: {
-    address: "Avenida Jurumirim",
-    bairro: "Bela Vista",
-    casa: "30",
-    cep: "78050-194",
-    reference: "Any reference",
-  },
-  name: "Felipe Aguiar",
-  phone: "(65) 99239-1563",
-};
-const order_to_update = new Order(28, 8, -1, client, ["A"], "Dinheiro, troco para R$50", "1buyci51ol8t623t7");
-const updateOrder = new UpdateOrder(order_to_update, { name: "Felipe Aguiar" });
-updateOrder.changeDeliveryPrice(6);
