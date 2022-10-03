4a72cece3a82b71e834d058750531d62b573de6a
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-10-03 05:50:44 -0400

    new test conifg and create-order funcionality

--- src/use-cases/create-order.ts
+++ src/use-cases/create-order.ts
@@ -1,6 +1,8 @@
-import { isEqual } from "lodash";
+import { collection, doc, getDocs, QuerySnapshot, setDoc } from "firebase/firestore";
+import { isEqual, uniqueId } from "lodash";
 import { Order } from "../entities/order";
 import { OrderError } from "../entities/order_error";
+import { DB } from "../firebase";
 
 export class CreateOrder {
   constructor(private readonly order: Order, orderRepository: Order[]) {
@@ -35,6 +37,33 @@ export class CreateOrder {
     });
   }
   public async execute() {
-    // Create the order inside here (No extra verification, only creates it)
+    try {
+      return setDoc(doc(DB, "Orders", this.order.id), Object.assign({}, this.order));
+    } catch (e: any) {
+      // Error handling
+    }
   }
 }
+
+/* const testRepo = getDocs(collection(DB, "Orders")) as Promise<QuerySnapshot<Order>>;
+
+testRepo.then((result) => {
+  const client = {
+    location: {
+      address: "Avenida Jurumirim",
+      bairro: "Bela Vista",
+      casa: "30",
+      cep: "78050-194",
+      reference: "Any reference",
+    },
+    name: "Felipe Aguiar",
+    phone: "(65) 99239-1563",
+  };
+  const order_to_go = new Order(28, 8, -1, client, ["A"], "Dinheiro, troco para R$50");
+  const new_order = new CreateOrder(
+    order_to_go,
+    result.docs.map((file) => file.data())
+  );
+  new_order.execute();
+});
+ */
