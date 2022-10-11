6bd157ae5547bc505b2313eae33dcca4c4704395
Sir-Aguiar <felipeferreiraaguiar9@gmail.com>
2022-10-11 16:52:01 -0400

    Fixing cookies type error

--- src/server/middlewares/ValidateEmployeeMiddleware.ts
+++ src/server/middlewares/ValidateEmployeeMiddleware.ts
@@ -22,34 +22,39 @@ const ValidateEmployeeMiddleware = async (req: Request, res: Response, next: Nex
     await signInWithEmailAndPassword(getAuth(firebase_app), email, password);
     console.log(`${emp_name} entry authorized at ${new Date().toLocaleString()}`);
     next();
-  } catch (error: any) {
-    const e = error as FirebaseError;
-    const error_infos = e.code.split("/");
+  } catch (e: any) {
+    if (e instanceof FirebaseError) {
+      const error_infos = e.code.split("/");
 
-    // Errors on function release
-    if (error_infos[0] == "invalid-argument") {
-      return res.status(500).json({ e });
-    }
-
-    // Errors on auth
-    if (error_infos[0] === "auth") {
-      switch (error_infos[1]) {
-        // Missing params
-        case "internal-error":
-          return res.status(400).json({ e: "A few information are missing" });
-
-        // Missing email
-        case "invalid-email":
-          return res.status(400).json({ e: "Missing email" });
-
-        // Invalid user
-        default:
-          return res.status(401).json({ e: "This user is not allowed to consume our services" });
+      // Errors on function release
+      if (error_infos[0] == "invalid-argument") {
+        return res.status(500).json({ e });
       }
-    }
 
-    // Unknown error
-    return res.status(510).json({ e: "An unknown error has been occurred during the execution of a functionality" });
+      // Errors on auth
+      if (error_infos[0] === "auth") {
+        switch (error_infos[1]) {
+          // Missing params
+          case "internal-error":
+            return res.status(400).json({ e: "A few information are missing" });
+
+          // Missing email
+          case "invalid-email":
+            return res.status(400).json({ e: "Missing email" });
+
+          // Invalid user
+          default:
+            return res.status(401).json({ e: "This user is not allowed to consume our services" });
+        }
+      }
+
+      // Unknown error
+      return res.status(510).json({ e: "An unknown error has been occurred during the execution of a functionality" });
+    }
+    if (e instanceof TypeError) {
+      return res.status(400).json({ e: { message: "Invalid cookies has been received" } });
+    }
+    return res.status(400).json({ e: { message: "An unknown error has occurred" } });
   }
 };
 export { RoutesForValidation, ValidateEmployeeMiddleware };
